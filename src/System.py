01d32113b78c0803152bff9c08e26c0a7e8de80b
danibt656 <dbmartin656@gmail.com>
2023-02-10 11:34:39 +0100

    experimentos con otro dataset gender_clf

--- src/System.py
+++ src/System.py
@@ -23,16 +23,16 @@ def get_dataloaders_from_path(path):
 
   data_transforms = {
     'train': transforms.Compose([
-      transforms.RandomResizedCrop(IMG_SIZE),
+      transforms.RandomSizedCrop(IMG_SIZE),
       transforms.RandomHorizontalFlip(),
       transforms.ToTensor(),
-      transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+      transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ]),
     'val': transforms.Compose([
-      transforms.Resize(IMG_SIZE),
+      transforms.Scale(256),
       transforms.CenterCrop(IMG_SIZE),
       transforms.ToTensor(),
-      transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+      transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ]),
   }
 
@@ -41,9 +41,9 @@ def get_dataloaders_from_path(path):
 
   dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x],\
                                                 batch_size=BATCH_SIZE,\
-                                                shuffle=True,\
+                                                shuffle=shuf,\
                                                 num_workers=4)\
-                  for x in ['train', 'val']}
+                  for x,shuf in [('train', True), ('val', False)]}
 
   dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
   class_names = [c for c in image_datasets['train'].classes if c != ROOT_I]
